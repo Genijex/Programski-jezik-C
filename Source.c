36bf2b5a642a4f94b77fb407cd51621752ec30f7
Filip Filipović <Filip Filipovi>
2016-11-06 14:45:38 +0100

    Added new function

--- Source.c
+++ Source.c
@@ -10,9 +10,19 @@ void prikazi(int niz[], int n){
 	printf("\n");
 }
 
+void prikaziZbir(int niz[], int n){
+	int i;	
+	int zbir = 0;
+	for(i = 0; i < n; i++){
+		zbir += niz[i];
+	}
+	printf("Zbir = %d\n",zbir);
+}
+
 int main() {
 	int niz[] = {1, 2, 3, 4, 5, 6};
 	int n = 6;
 
 	prikazi(niz, n);
+	prikaziZbir(niz, n);
 }
